31a66b29ef4e5d817877f7ff8497285786a7613f
matheus dias <matheusmelhor3@outlook.com>
2022-03-11 16:23:14 -0300

    Project Created

---
+++ src/test/java/br/matheus/dias/curso/CursoudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.matheus.dias.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
